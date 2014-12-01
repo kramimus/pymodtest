d8153b03ab7b2af0c8caa67dc22ea03f04a933de
Mark G. Whitney <markwhitney@gmail.com>
2014-11-30 23:02:23 -0800

    changed to prosepector, fixed a bunch of file list parsing bugs

--- pymodtest.sh
+++ pymodtest.sh
@@ -1,25 +1,71 @@
 #!/bin/bash
 
+show_help() {
+    cat <<EOF
+Usage: ${0##*/} [-hcs]
+
+    -h Show this help
+
+    -s Check staged files
+
+    -c Check files in previous commit
+EOF
+}
+
+filter_existing() {
+    pyfiles=$@
+    existingfiles=""
+    for f in $pyfiles; do
+        if [ -e $f ]; then
+            existingfiles=$(echo "$existingfiles $f")
+        fi
+    done
+    echo "$existingfiles"
+}
+
+get_test_packages() {
+    for f in $pyfiles; do
+        if [[ "$f" =~ "/test_" ]]; then
+            totest[${f/\.py/}]=1
+        elif [[ "$f" =~ "__init__" ]]; then
+            echo "skipping $f"
+        else
+            withtestpath=${f/\//\/tests\/}
+            withtestprefix=$(echo $withtestpath | sed 's/\([a-zA-Z_]*\)\.py/test_\1/')
+            if [ -e "${withtestprefix}.py" ]; then
+                totest[$withtestprefix]=1
+            fi
+        fi
+    done
+    echo "${!totest[@]}"
+}
+
+
+while getopts ":sc" opt; do
+    case $opt in
+        c)
+            files=$(git diff HEAD^ --name-only)
+            ;;
+        s)
+            files=$(git diff --cached --name-only)
+            ;;
+    esac
+done
+
+if [ -z "$files" ]; then
+    show_help
+    exit -1
+fi
+
+pyfiles=$(echo "$files" | grep ".py$" | grep -v "/migrations/")
 declare -A totest
 
-pyfiles=$(git ls-files --others -m -c | grep ".py$")
-for f in $pyfiles; do
-    if [[ "$f" =~ "/test_" ]]; then
-        totest[${f/\.py/}]=1
-    elif [[ "$f" =~ "__init__" ]]; then
-        echo "skipping $f"
-    else
-        withtestpath=${f/\//\/tests\/}
-        withtestprefix=$(echo $withtestpath | sed 's/\([a-zA-Z_]*\)\.py/test_\1/')
-        totest[$withtestprefix]=1
-    fi
-done
-all=${!totest[@]}
+echo $pyfiles
+pyfiles=$(filter_existing $pyfiles)
+all=$(get_test_packages $pyfiles)
 
 autopep8 -i --max-line-length=80 $pyfiles
-git-lint
-python -m unittest ${all//\//\.}
+prospector | grep -E -A2 $(echo "${pyfiles// /|}" | sed 's/^|//' | sed 's/|$//')
 
-
-
-
+echo ${all//\//\.}
+~/rescale/rescale-platform-web/manage.py test ${all//\//\.}
